1e16623db4b70775480bde3c212b6718d82e63a8
bahooo <bahooo@bb.com>
2017-10-25 00:05:12 +0300

    working version of processing without email sending

--- task10/process.php
+++ task10/process.php
@@ -1,39 +1,53 @@
 <?php
-// видалення символів переносу рядка з тексту переданого через POST
-$arr = str_replace("\n", " ", $_POST["input"]);
-// створення массиву із розділенням по пробілу
-$arrFormat = explode(" ", $arr);
+// створення масиву формату [ 0 => email прізвище ім'я по-батькові]
+$arrFormat = explode("\n", $_POST["input"]);
 
 print_r($arrFormat);
 echo "<br><br>";
 
-$arrGood=[];
-// cтворення массиву формату [email=>[lastName, name, patronymic]
-foreach ($arrFormat as $key => $value) {
-	if ((int)$key%4==0) {
-		$index = $value;
-		$arrGood[$index]=[];
+// перемішування массиву
+$arrAssoc = shuffle_assoc($arrFormat); 
+function shuffle_assoc($array) {
+	$keys = array_keys($array);
+	shuffle($keys);
+	foreach($keys as $key => $value) {
+		$new[$key] = $array[$value];
+    }
+    $array = $new;
+    return $array;
+}
+
+print_r($arrAssoc);
+echo "<br><br>";
+
+// створення масиву формату [ 0 => [email, прізвище, ім'я, по-батькові]]
+foreach ($arrAssoc as $key => $value) {
+	$arrAssoc[$key]=explode(" ", trim($arrAssoc[$key]));
+}
+
+var_dump($arrAssoc);
+echo "<br><br>";
+
+// створення і запис інформації у файли
+foreach ($arrAssoc as $key => $value) {
+	if ($key == count($arrAssoc)-1) {
+		$file = fopen("$value[1]_$value[2]_$value[3].txt", "w");
+		fwrite($file, $arrAssoc[0][1]." ".$arrAssoc[0][2]." ".$arrAssoc[0][3]);
+		fclose($file);
 	} else {
-		array_push($arrGood[$index], $value);
+		$file = fopen("$value[1]_$value[2]_$value[3].txt", "w");
+		fwrite($file, $arrAssoc[$key+1][1]." ".$arrAssoc[$key+1][2]." ".$arrAssoc[$key+1][3]);
+		fclose($file);
 	}
 }
 
-// перетворення массиву формату [email=>[lastName, name, patronymic] до формату [email=>fullName]
-foreach ($arrGood as $key => $value) {
-	$arrGood[$key]=implode(" ", $arrGood[$key]);
-}
 
-print_r($arrGood);
-echo "<br><br>";
+// function shuffle_assoc($array) 
+// { 
+//    $keys = array_keys($array); 
+//    shuffle($keys); 
+//    return array_merge(array_flip($keys) , $array); 
+// }
 
-$arrAssoc = shuffle_assoc( $arrGood ); 
-// функція перемішування массиву
-function shuffle_assoc($array) 
-{ 
-   $keys = array_keys($array); 
-   shuffle($keys); 
-   return array_merge(array_flip($keys) , $array); 
-}
-print_r($arrAssoc);
-echo "<br><br>";
+//https://habrahabr.ru/sandbox/4875/
 ?>
