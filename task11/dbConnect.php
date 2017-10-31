37920086664f67d1fb0d1f79764df09d778f7f11
bahooo <bahooo@bb.com>
2017-10-31 16:42:48 +0200

    removed data query

--- task11/dbConnect.php
+++ task11/dbConnect.php
@@ -8,21 +8,8 @@ $dbUser = "root";
 $dbPass = "0000";
 
 $dbLink = mysqli_connect("localhost", $dbUser, $dbPass, $dbName);
+mysqli_query($dbLink,"SET NAMES 'utf8'"); 
+mysqli_query($dbLink,"SET CHARACTER SET 'utf8'");
+mysqli_query($dbLink,"SET SESSION collation_connection = 'utf8_general_ci'");
 
-$name = "user3";
-$pas = "2222";
-$group_id = 3;
-
-$sql = "INSERT INTO users (nickname, pass, group_id) VALUES (?, ?, ?)";
-$stmt = mysqli_stmt_init($dbLink);
-mysqli_stmt_prepare($stmt, $sql);
-mysqli_stmt_bind_param ($stmt, "ssi", $name, $pas, $group_id);
-mysqli_stmt_execute($stmt);
-// $result = mysqli_stmt_get_result($stmt);
-// $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-// var_dump($row);
-
-mysqli_stmt_close($stmt);
-mysqli_close($dbLink);
 ?>
