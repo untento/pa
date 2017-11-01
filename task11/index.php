f4fcd1125f36dc24fd490cf982ce01a6eebeaf67
bahooo <bahooo@bb.com>
2017-11-01 16:16:49 +0200

    added post author and creation date

--- task11/index.php
+++ task11/index.php
@@ -1,18 +1,17 @@
-<?php require 'header.php'; ?>
+<?php require 'header.php';?>
 
 <?php
 require 'dbConnect.php';
 
-
-
-$sql = "SELECT * FROM posts";
+$sql = "SELECT posts.post_id, posts.post_name, posts.post_text, posts.date, users.nickname FROM posts INNER JOIN users ON posts.user_id=users.user_id ORDER BY posts.date DESC";
 $stmt = mysqli_stmt_init($dbLink);
 mysqli_stmt_prepare($stmt, $sql);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
  while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
  	$postID = $row["post_id"];
- 	printf ("<b><a href=\"post.php?post_id=$postID\">%s</a></b> <br> %s <br><br> ", $row["post_name"], $row["post_text"]);
+ 	printf ("<b><a href=\"post.php?post_id=$postID\">%s</a></b> <br> %s", $row["post_name"], $row["post_text"]);
+ 	printf("<i>Створено користувачем %s, %s </i><br><br><br>", $row["nickname"], date("d.m.Y", strtotime($row["date"])));
  }
 
 mysqli_stmt_close($stmt);
--- task11/post.php
+++ task11/post.php
@@ -3,14 +3,15 @@
 <?php
 require 'dbConnect.php';
 
-$sql = "SELECT post_name, post_text FROM posts WHERE post_id=?";
+$sql = "SELECT posts.post_id, posts.post_name, posts.post_text, posts.date, users.nickname FROM posts INNER JOIN users ON posts.user_id=users.user_id WHERE posts.post_id=?";
 $stmt = mysqli_stmt_init($dbLink);
 mysqli_stmt_prepare($stmt, $sql);
 mysqli_stmt_bind_param($stmt, "i", $_GET['post_id']);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
  while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
- 	printf ("<b>%s</b> <br> %s <br><br> ", $row["post_name"], $row["post_text"]);
+ 	printf ("<b>%s</b> <br> %s <br> ", $row["post_name"], $row["post_text"]);
+ 	printf("Створено користувачем %s, %s", $row["nickname"], date("d.m.Y", strtotime($row["date"])));
  }
 
 mysqli_stmt_close($stmt);
