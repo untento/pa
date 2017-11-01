25bd2ebe859149568b8760c7af7f6a879ac645a9
bahooo <bahooo@bb.com>
2017-11-01 16:47:10 +0200

    menu list generator

---
+++ task11/menu.php
@@ -0,0 +1,12 @@
+<?php
+
+require ("config.php");
+CreateMenu($menu_list);
+
+function CreateMenu($list) {
+	foreach ($list as $key => $value) {
+		echo '<li><a href='.$key.'>'.$value.'</a></li>';
+	}
+}
+
+?>
