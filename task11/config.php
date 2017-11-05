3ff2031815e936c6f9bb6d203d8496af1faae942
bahooo <bahooo@bb.com>
2017-11-05 20:05:27 +0200

    added sign out menu

--- task11/config.php
+++ task11/config.php
@@ -1,7 +1,13 @@
 <?php
 
-$menu_list = ["index.php" => "Головна",			  
+if (isset($_SESSION['id'])) {
+	$menu_list = ["index.php" => "Головна",			  
+			  "signOut.php" => "Вийти"
+			 ];
+} else {
+	$menu_list = ["index.php" => "Головна",			  
 			  "signIn.php" => "Авторизація"
 			 ];
-			 
+}
+
 ?>
