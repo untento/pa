4aa58d10ffc05d2d746b7bd21f8b0258c1c85d56
bahooo <bahooo@bb.com>
2017-11-05 20:09:41 +0200

    script for sign in verification

---
+++ task11/verification.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+// print_r($_POST);
+$credentials = $_POST;
+
+require 'dbConnect.php';
+
+$sql = "SELECT user_id, nickname, pass FROM users WHERE nickname=?";
+$stmt = mysqli_stmt_init($dbLink);
+mysqli_stmt_prepare($stmt, $sql);
+mysqli_stmt_bind_param($stmt, "i", $credentials['nickname']);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$userData = (mysqli_fetch_array($result, MYSQLI_ASSOC));
+mysqli_stmt_close($stmt);
+mysqli_close($dbLink);
+
+if ($credentials['nickname'] == $userData['nickname'] AND $credentials['pass'] == $userData['pass']) {
+	$_SESSION['id'] = $userData['user_id'];
+	echo 'Ви успішно залогінились <br>';
+	echo '<a href="index.php">Повернутися на головну сторінку</a>';
+} else {
+	echo "bad";
+}
+?>
