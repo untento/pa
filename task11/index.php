076176d1c20a9be8aea0522b2c97dbd6c1b31d50
bahooo <bahooo@bb.com>
2017-12-13 19:41:06 +0200

    edited posts list

--- task11/index.php
+++ task11/index.php
@@ -3,7 +3,7 @@
 <?php
 require 'dbConnect.php';
 
-$sql = "SELECT posts.post_id, posts.post_name, posts.post_text, posts.date, users.nickname FROM posts INNER JOIN users ON posts.user_id=users.user_id ORDER BY posts.date DESC";
+$sql = "SELECT posts.post_id, posts.post_name, posts.post_description, posts.date, users.nickname FROM posts INNER JOIN users ON posts.user_id=users.user_id ORDER BY posts.date DESC";
 $stmt = mysqli_stmt_init($dbLink);
 mysqli_stmt_prepare($stmt, $sql);
 mysqli_stmt_execute($stmt);
@@ -12,9 +12,11 @@ echo "<div class=\"parent\">";
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 	$postID = $row["post_id"];
 	echo "<div class=\"block\">";
-	printf ("<b><a href=\"post.php?post_id=$postID\">%s</a></b> <br> %s", $row["post_name"], $row["post_text"]);
+	printf ("<b><a href=\"post.php?post_id=$postID\">%s</a></b> <br> <div class=\"box\">%s ", $row["post_name"], $row["post_description"]);
+	echo "</div>";
 	printf("<b><i>Створено користувачем %s, %s </i></b><br>", $row["nickname"], date("d.m.Y", strtotime($row["date"])));
 	echo "</div>";
+
 }
 echo "</div>";
 mysqli_stmt_close($stmt);
