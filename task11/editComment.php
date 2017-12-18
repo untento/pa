878e2fbe9de5e6b25546e5fcea308c760c2480f8
bahooo <bahooo@bb.com>
2017-12-18 14:14:34 +0200

    comment editing form

---
+++ task11/editComment.php
@@ -0,0 +1,30 @@
+<?php require 'header.php';?>
+
+<?php
+
+require 'dbConnect.php';
+$_SESSION['comment_id'] = $_GET['comment_id'];
+$sql = "SELECT comments.comment FROM comments WHERE comments.comment_id=?";
+$stmt = mysqli_stmt_init($dbLink);
+mysqli_stmt_prepare($stmt, $sql);
+mysqli_stmt_bind_param($stmt, "i", $_GET['comment_id']);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+?>
+<form action="editComm.php" method="POST">
+    <div class="modal-body">
+        <div class="form-group">
+            <label>Редагування коментаря</label>
+            <textarea class="form-control focusedInput" rows="5" name="comment"><?php echo $row["comment"];?></textarea>
+            <button type="submit" class="btn btn-success">Відредагувати</button>
+            <input class="btn btn-danger" type="button" value="Відміна" onClick='location.href="post.php?post_id=<?php echo $_GET['post_id']; ?>"'>
+        </div>
+    </div>
+</form>
+
+<?php
+                                                         }
+?>
+
+<?php require 'footer.php'; ?>
