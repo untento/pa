57b922847c26c4d687e027523c6c52cc3f169830
bahooo <bahooo@bb.com>
2017-12-18 14:15:59 +0200

    comment removing form

---
+++ task11/removeComment.php
@@ -0,0 +1,28 @@
+<?php require 'header.php';?>
+
+<?php
+require 'dbConnect.php';
+$_SESSION['comment_id'] = $_GET['comment_id'];
+$_SESSION['post_id'] = $_GET['post_id'];
+$sql = "SELECT comments.comment FROM comments WHERE comments.comment_id=?";
+$stmt = mysqli_stmt_init($dbLink);
+mysqli_stmt_prepare($stmt, $sql);
+mysqli_stmt_bind_param($stmt, "i", $_GET['comment_id']);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+?>
+<form action="removeComm.php" method="POST">
+    <div class="modal-body">
+        <div class="form-group">
+            <label>Видалити коментар:</label>
+            <p><?php echo $row["comment"];?></p>
+            <button type="submit" class="btn btn-success">Видалити</button>
+            <input class="btn btn-danger" type="button" value="Відміна" onClick='location.href="post.php?post_id=<?php echo $_GET['post_id']; ?>"'> 
+        </div>
+    </div>
+</form>
+<?php
+                                                         }
+?>
+<?php require 'footer.php'; ?>
